2b19cdbe6af9cc8a4696b6ba3c22cdf9ae265aa8
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 <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>
2024-03-28 14:01:39 -0400

    $[Dr.Wise]@(Kali)//:{Holberton}$

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""a sccript tu run a web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """root path"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """path hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """handles the C path"""
+    formatted_text = text.replace("_", " ")
+    return f"C {formatted_text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def display_python(text="is cool"):
+    """handles the python path"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
