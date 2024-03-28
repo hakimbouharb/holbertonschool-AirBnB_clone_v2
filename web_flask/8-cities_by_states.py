9eab1ce6f29d1905eb4d1affb9ce16d68060fbf0
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 <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>
2024-03-28 14:22:12 -0400

    $[Dr.Wise]@(Kali)//:{Holberton}$

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/python3
+"""a script that starts a web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display an html page with a list of all states"""
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current sqlalchemy session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
