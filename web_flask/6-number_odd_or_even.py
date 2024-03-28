9a64d898703d263a259bdfa47f4f22db25a1e891
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 <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>
2024-03-28 14:09:50 -0400

    $[Dr.Wise]@(Kali)//:{Holberton}$

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""a script that starts aFlask web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """root path"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """path hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """handles the C path"""
+    formatted_text = text.replace("_", " ")
+    return f"C {formatted_text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def display_python(text="is cool"):
+    """handles the python path"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """number path"""
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_html(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
